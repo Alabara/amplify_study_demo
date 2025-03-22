baf4d4bd5d6037a08cac3b92b6edbc0630516e87
Alabara <wzw4432@163.com>
2025-03-22 19:48:24 +0800

    init

---
+++ src/graphql/queries.js
@@ -0,0 +1,44 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const callMyFunction = /* GraphQL */ `
+  query CallMyFunction($input: MyFunctionInput) {
+    callMyFunction(input: $input) {
+      statusCode
+      body
+      __typename
+    }
+  }
+`;
+export const getTodo = /* GraphQL */ `
+  query GetTodo($id: ID!) {
+    getTodo(id: $id) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const listTodos = /* GraphQL */ `
+  query ListTodos(
+    $filter: ModelTodoFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listTodos(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        description
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
